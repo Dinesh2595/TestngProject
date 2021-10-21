f46c022b796847c543f897ffd3766ce4dad582be
Dinesh2595 <mldinesh25@gmail.com>
2021-10-21 19:26:10 +0530

    TestngProject

---
+++ src/test/java/org/TestngSample/FacebookInfo.java
@@ -0,0 +1,13 @@
+package org.TestngSample;
+
+import org.testng.annotations.Test;
+
+public class FacebookInfo {
+
+	@Test
+	private void test1() {
+		System.out.println("test1");
+		System.out.println("Test2");
+	}
+
+}
